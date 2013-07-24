a8afebe8318a5fa83b76cf4e94d63e6741b93994
Mariano Guerra <mariano@marianoguerra.org>
2013-07-24 15:44:25 +0200

    0.1.1-SNAPSHOT: use public repos for deps that are available

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marianoguerra/google-docs-spreadsheets "0.1.0"
+(defproject marianoguerra/google-docs-spreadsheets "0.1.1-SNAPSHOT"
   :description "Library to interact with google spreadsheets"
   :url "http://github.com/marianoguerra/google-docs-spreadsheets"
   :license {:name "Eclipse Public License"
@@ -6,16 +6,17 @@
   :main marianoguerra.google-docs-spreadsheets-cli/main
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.2"]
+
                  [gdata-core/gdata-core "1.0"]
                  [gdata-base/gdata-base "1.0"]
                  [gdata-client/gdata-client "1.0"]
                  [gdata-client-meta/gdata-client-meta "1.0"]
-                 [google-oauth-client/google-oauth-client "1.15.0-rc"]
-                 [google-http-client/google-http-client "1.15.0-rc"]
-
                  [gdata-docs/gdata-docs "3.0"]
                  [gdata-docs-meta/gdata-docs-meta "3.0"]
                  [gdata-spreadsheet/gdata-spreadsheet "3.0"]
                  [gdata-spreadsheet-meta/gdata-spreadsheet-meta "3.0"]
-                 [guava/guava "11.0.2"]])
+
+                 [com.google.oauth-client/google-oauth-client "1.15.0-rc"]
+                 [com.google.http-client/google-http-client "1.15.0-rc"]
+                 [com.google.guava/guava "11.0.2"]])
 
